65bd8fa2d9dcc4a194875517c8722c6b56b3122f
Hernán Alegresa <135678248+HernanAlegresa@users.noreply.github.com>
2023-11-20 13:05:46 -0300

    print_char.c function

--- print_char.c
+++ print_char.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_char - Print a character.
- * @args: Argument list containing the character to be printed.
- * Return: Number of characters printed.
+ * print_char - Print a character
+ * @args: Argument list containing the character to be printed
+ * Return: Number of characters printed
  */
 int print_char(va_list args)
 {
