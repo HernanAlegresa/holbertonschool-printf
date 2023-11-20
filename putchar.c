ab0d40f0ea1040c58c292a0383f33424938626fe
Hernán Alegresa <135678248+HernanAlegresa@users.noreply.github.com>
2023-11-20 10:54:55 -0300

    Update putchar.c

--- putchar.c
+++ putchar.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 
+/**
+ * _putchar - Print a single character.
+ * @c: The character to be printed.
+ * Return: 1 on success, -1 on failure.
+ */
 int _putchar(char c)
 {
     return write(1, &c, 1);
