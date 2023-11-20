086a34a59314e57a51c150c83c260b3103dc8a5f
Hernán Alegresa <135678248+HernanAlegresa@users.noreply.github.com>
2023-11-20 13:07:36 -0300

    putchar.c function

--- putchar.c
+++ putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- * _putchar - Print a single character.
- * @c: The character to be printed.
- * Return: 1 on success, -1 on failure.
+ * _putchar - Print a single character
+ * @c: The character to be printed
+ * Return: 1 on success, -1 on failure
  */
 int _putchar(char c)
 {
